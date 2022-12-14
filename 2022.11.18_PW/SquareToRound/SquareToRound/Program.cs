ca42a92e336026750ea05bc89d15dfdec38f91d5
Rhoxolan <pba17mail@gmail.com>
2022-12-14 19:40:16 +0200

    Add Practic Work

---
+++ 2022.11.18_PW/SquareToRound/SquareToRound/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SquareToRound
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            RoundHole hole = new RoundHole
+            {
+                D = 10
+            };
+
+            SquarePeg squarePeg = new SquarePeg
+            {
+                Side = 5
+            };
+
+            RoundPeg roundPeg = new SquareToRoundAdapter(squarePeg);
+
+            Console.WriteLine(hole.Fits(roundPeg));
+        }
+    }
+
+    class RoundHole //Client
+    {
+        public required double D { get; init; }
+
+        public bool Fits(RoundPeg roundPeg)
+            => roundPeg.D <= D;
+    }
+
+    class SquarePeg //Adaptee
+    {
+        public required double Side { get; set; }
+    }
+
+    class RoundPeg //Target
+    {
+        public required double D { get; set; }
+    }
+
+    class SquareToRoundAdapter : RoundPeg //Adapter
+    {
+        [SetsRequiredMembers]
+        public SquareToRoundAdapter(SquarePeg squarePeg)
+        {
+            D = squarePeg.Side / Math.Sqrt(2) * 2;
+        }
+    }
+
+}
